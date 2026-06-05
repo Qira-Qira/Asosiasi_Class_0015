264aca3f871ae9c4fb03e5e5e47ecb877bd6fa54
Qira-Qira <nawafariq3@gmail.com>
2026-06-05 17:35:30 +0700

    Membuat constructor

--- Main.java
+++ Main.java
@@ -19,5 +19,14 @@ class MataKuliah {
     private String nama;
     private int sks;
 
+    public MataKuliah(String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+    }
+
+    public String getInfo() {
+        return kode + " - " + nama + " (" + sks + " SKS)";
+    }
 }
 
