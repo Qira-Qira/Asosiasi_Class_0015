afb3c175f820057ca81c74265a6dd319eac4750d
Qira-Qira <nawafariq3@gmail.com>
2026-06-05 16:30:35 +0700

    Membuat Abstract Class Orang

--- Main.java
+++ Main.java
@@ -1,1 +1,16 @@
 import java.util.ArrayList;
+
+abstract class Orang {
+    private String nama;
+
+    public Orang(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public abstract void tampilkanPeran();
+}
+
